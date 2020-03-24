d723e7e7621b8d826ef5f44a48ffbc8c05ef0bb8
imane <benbenimane@hotmail.com>
2020-03-24 20:26:42 +0100

    hmmmm how to count on reverse..

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,47 @@ import '../App.css';
 
 function App() {
 
-    const[startMinutes,  setCounter]  = useState(25);
-    const[startSeconds,  setCount]  = useState(0);
+    const[startMinutes,  setMinute]  = useState(25);
+    let [startSeconds,  setSec]  = useState(0);
 
-    const increment =() =>
-    {
+    // add minutes
+    const  addMinute =()=>{
+       setMinute (startMinutes +1)
+    };
 
-        setCount(startSeconds  +1);
+    const  minMinute =()=>{
+        //if < 0 stop -1
+        setMinute (startMinutes <= 0 ? 0 :  startMinutes -1 )
+    };
+
+    const  addSec =()=>{
+        //if < 0 stop -1
+        setSec (startSeconds   - 1  )
 
     };
 
-    setInterval(increment,1000);
+
+    const increment = ()=>{
+       setInterval(addSec,1000);
+    };
+
+    const stopIncrement = ()=>{
+
+       clearInterval(increment)
+    };
+
+
   return (
 
     <div className="App">
         <h2>Pomodoro Clock</h2>
         <Timer Minutes={startMinutes} Seconds={startSeconds}  />
 
+        <button onClick={addMinute}>add Minute</button>
+        <button onClick={minMinute}>Min minute</button>
         <button onClick={increment}>start</button>
+        <button onClick={stopIncrement}>stop</button>
+
     </div>
   );
 }
